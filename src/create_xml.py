3cba94f76c3556b4b16520837d66c8cbe5638a69
Satoru Nakamura <na.kamura.1263@gmail.com>
2020-01-19 22:47:59 +0900

    add 9 and 10, and bug fix

--- src/create_xml.py
+++ src/create_xml.py
@@ -36,125 +36,127 @@ def get_mdata(manifest):
 
     return map
 
+vols = [3, 4, 5, 6, 7, 8, 9, 10, 11]
 
-prefix = ".//{http://www.tei-c.org/ns/1.0}"
-xml = ".//{http://www.w3.org/XML/1998/namespace}"
+for vol in vols:
 
-tmp_path = "data/template.xml"
+    prefix = ".//{http://www.tei-c.org/ns/1.0}"
+    xml = ".//{http://www.w3.org/XML/1998/namespace}"
 
-tree = ET.parse(tmp_path)
-ET.register_namespace('', "http://www.tei-c.org/ns/1.0")
-ET.register_namespace('xml', "http://www.w3.org/XML/1998/namespace")
-root = tree.getroot()
+    tmp_path = "data/template.xml"
 
-para = root.find(prefix + "body").find(prefix + "p")
+    tree = ET.parse(tmp_path)
+    ET.register_namespace('', "http://www.tei-c.org/ns/1.0")
+    ET.register_namespace('xml', "http://www.w3.org/XML/1998/namespace")
+    root = tree.getroot()
 
-files = glob.glob("../data/*.json")
+    para = root.find(prefix + "body").find(prefix + "p")
 
-surfaceGrp = root.find(prefix+"surfaceGrp")
+    files = glob.glob("../data/*.json")
 
-manifest = surfaceGrp.get("facs")
+    surfaceGrp = root.find(prefix+"surfaceGrp")
 
-canvas_data = get_mdata(manifest)
+    manifest = surfaceGrp.get("facs")
 
-prev_page = -1
+    canvas_data = get_mdata(manifest)
 
-canvas_map = {}
+    prev_page = -1
 
-vol = 11
+    canvas_map = {}
 
-for file in sorted(files):
 
-    with open(file, 'r') as f:
-        data = json.load(f)
+    for file in sorted(files):
 
-        value = data[0]["http://www.w3.org/2000/01/rdf-schema#label"][0]["@value"]
+        with open(file, 'r') as f:
+            data = json.load(f)
 
-        if "http://example.org/冊数名" not in data[0]:
-            continue
+            value = data[0]["http://www.w3.org/2000/01/rdf-schema#label"][0]["@value"]
 
-        vol_ = data[0]["http://example.org/冊数名"][0]["@value"]
+            if "http://example.org/冊数名" not in data[0]:
+                continue
 
-        if vol != vol_:
-            continue
+            vol_ = data[0]["http://example.org/冊数名"][0]["@value"]
 
-        title = data[0]["http://example.org/巻名"][0]["@value"]
-        root.find(prefix + "title").text = str(vol).zfill(2) + " "+ title
+            if vol != vol_:
+                continue
 
-        id = data[0]["@id"]
+            title = data[0]["http://example.org/巻名"][0]["@value"]
+            root.find(prefix + "title").text = str(vol).zfill(2) + " "+ title
 
-        page = data[0]["http://example.org/頁数"][0]["@value"]
+            id = data[0]["@id"]
 
-         # 新しい頁
-        if page != prev_page:
-            prev_page = page
+            page = data[0]["http://example.org/頁数"][0]["@value"]
+
+            # 新しい頁
+            if page != prev_page:
+                prev_page = page
+
+                lb = ET.Element(
+                    "{http://www.tei-c.org/ns/1.0}lb")
+                para.append(lb)
+
+                pb = ET.Element(
+                    "{http://www.tei-c.org/ns/1.0}pb")
+                pb.set("n", str(page))
+                pb.set("facs", "#zone_"+str(page).zfill(4))
+                para.append(pb)
+
+                canvas_index = 20 + int(page / 2)
+                canvas_id = "https://www.dl.ndl.go.jp/api/iiif/3437686/canvas/"+str(canvas_index)
+                
+                obj = canvas_data[canvas_id]
+
+                if canvas_id not in canvas_map:
+                    canvas_map[canvas_id] = {
+                        "url": obj["url"],
+                        "zones": []
+                    }
+
+                if page % 2 == 0:
+                    lrx = obj["width"]
+                    ulx = int(lrx / 2)
+                else:
+                    lrx = int(obj["width"] / 2)
+                    ulx = 0
+
+                zone = ET.Element(
+                    "{http://www.tei-c.org/ns/1.0}zone")
+                zone.set("xml:id", "zone_"+str(page).zfill(4))
+                zone.set("lrx", str(lrx))
+                zone.set("lry", str(obj["height"]))
+                zone.set("ulx", str(ulx))
+                zone.set("uly", str(0))
+
+                canvas_map[canvas_id]["zones"].append(zone)
+
 
             lb = ET.Element(
-                "{http://www.tei-c.org/ns/1.0}lb")
+                    "{http://www.tei-c.org/ns/1.0}lb")
             para.append(lb)
 
-            pb = ET.Element(
-                "{http://www.tei-c.org/ns/1.0}pb")
-            pb.set("n", str(page))
-            pb.set("facs", "#zone_"+str(page).zfill(4))
-            para.append(pb)
+            line = ET.Element(
+                    "{http://www.tei-c.org/ns/1.0}seg")
+            line.set("corresp", id)
+            line.text = value
+            # para.append(line)
+            para.append(line)
 
-            canvas_index = 20 + int(page / 2)
-            canvas_id = "https://www.dl.ndl.go.jp/api/iiif/3437686/canvas/"+str(canvas_index)
-            
-            obj = canvas_data[canvas_id]
+    for canvas_id in canvas_map:
 
-            if canvas_id not in canvas_map:
-                canvas_map[canvas_id] = {
-                    "url": obj["url"],
-                    "zones": []
-                }
+        obj = canvas_map[canvas_id]
 
-            if page % 2 == 0:
-                lrx = obj["width"]
-                ulx = int(lrx / 2)
-            else:
-                lrx = int(obj["width"] / 2)
-                ulx = 0
+        surface = ET.Element(
+                    "{http://www.tei-c.org/ns/1.0}surface")
+        surfaceGrp.append(surface)
 
-            zone = ET.Element(
-                "{http://www.tei-c.org/ns/1.0}zone")
-            zone.set("xml:id", "zone_"+str(page).zfill(4))
-            zone.set("lrx", str(lrx))
-            zone.set("lry", str(obj["height"]))
-            zone.set("ulx", str(ulx))
-            zone.set("uly", str(0))
+        graphic = ET.Element(
+                    "{http://www.tei-c.org/ns/1.0}graphic")
+        graphic.set("n", canvas_id)
+        graphic.set("url", obj["url"])
+        surface.append(graphic)
 
-            canvas_map[canvas_id]["zones"].append(zone)
+        for zone in obj["zones"]:
+            surface.append(zone)
 
 
-        lb = ET.Element(
-                "{http://www.tei-c.org/ns/1.0}lb")
-        para.append(lb)
-
-        line = ET.Element(
-                "{http://www.tei-c.org/ns/1.0}seg")
-        line.set("corresp", id)
-        line.text = value
-        # para.append(line)
-        para.append(line)
-
-for canvas_id in canvas_map:
-
-    obj = canvas_map[canvas_id]
-
-    surface = ET.Element(
-                "{http://www.tei-c.org/ns/1.0}surface")
-    surfaceGrp.append(surface)
-
-    graphic = ET.Element(
-                "{http://www.tei-c.org/ns/1.0}graphic")
-    graphic.set("n", canvas_id)
-    graphic.set("url", obj["url"])
-    surface.append(graphic)
-
-    for zone in obj["zones"]:
-        surface.append(zone)
-
-
-tree.write("../tei/"+str(vol).zfill(2)+".xml", encoding="utf-8")
+    tree.write("../tei/"+str(vol).zfill(2)+".xml", encoding="utf-8")
